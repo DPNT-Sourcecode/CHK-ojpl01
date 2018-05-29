68bf8a9173bc927f12288241d6d53eb70938742f
sbx_user1060 <sbx_user1060@ip-10-12-61-131.eu-west-2.compute.internal>
2018-05-29 19:23:18 +0000

    Tue May 29 19:23:18 UTC 2018

--- lib/solutions/checkout.py
+++ lib/solutions/checkout.py
@@ -8,7 +8,7 @@ def checkout(skus):
     sum_ = 0
     a = 0
     b = 0
-
+    e = 0
     for item in skus:
 
         if item == "A" and a != 2:
@@ -21,12 +21,18 @@ def checkout(skus):
             sum_ += 20
         elif item == "D":
             sum_ += 15
+        elif item == "E" and e != 1:
+            sum_ += 40
+            e += 1
         elif item == "A" and a == 2:
             sum_ += 30
             a = 0
         elif item == "B" and b == 1:
             sum_ += 15
             b = 0
+        elif item == "E" and e == 1:
+            sum_ -= 30
+            e = 0
         else:
             return -1
     return int(sum_)
