fee6035d22e722dcc974992bd91b0e357846a9c4
sbx_user1060 <sbx_user1060@ip-10-12-61-131.eu-west-2.compute.internal>
2018-05-29 18:52:22 +0000

    Tue May 29 18:52:22 UTC 2018

--- lib/solutions/sum.py
+++ lib/solutions/sum.py
@@ -1,3 +1,13 @@
 # noinspection PyShadowingBuiltins,PyUnusedLocal
 def sum(x, y):
-    raise NotImplementedError()
+
+    try:
+        val_x = int(x)
+        val_y = int(y)
+    except ValueError:
+        raise
+
+    assert val_x >= 0 and val_x <= 100, "Value must be between 0 and 100"
+    assert val_y >= 0 and val_y <= 100, "Value must be between 0 and 100"
+
+    return (val_x + val_y)
