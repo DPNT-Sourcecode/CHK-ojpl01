e783b77d065d2aa227d6138b9ac9ddb686a9190c
sbx_user1060 <sbx_user1060@ip-10-12-61-131.eu-west-2.compute.internal>
2018-05-29 19:16:02 +0000

    Tue May 29 19:16:02 UTC 2018

--- lib/solutions/checkout.py
+++ lib/solutions/checkout.py
@@ -2,5 +2,29 @@
 
 # noinspection PyUnusedLocal
 # skus = unicode string
+
 def checkout(skus):
-    raise NotImplementedError()
+
+    sum_ = 0
+    a = 0
+    b = 0
+    for item in skus:
+
+        if item == "A" and a != 3:
+            sum_ += 50
+            a += 1
+        elif item == "B" and b != 2:
+            sum_ += 30
+            b += 1
+        elif item == "C":
+            sum_ += 20
+        elif item == "D":
+            sum_ += 15
+        elif item == "A" and a == 3:
+            sum_ += 30
+            a = 0
+        elif item == "B" and b == 2:
+            sum_ += 15
+            b = 0
+
+    return int(sum_)
