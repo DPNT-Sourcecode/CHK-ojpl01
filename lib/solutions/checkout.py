33979a02f62f121c24eb3cb13adfd1e083bbacfd
sbx_user1060 <sbx_user1060@ip-10-12-61-131.eu-west-2.compute.internal>
2018-05-29 20:05:18 +0000

    Tue May 29 20:05:18 UTC 2018

--- lib/solutions/checkout.py
+++ lib/solutions/checkout.py
@@ -37,7 +37,17 @@ def checkout(skus):
             sum_ += threes * 130
             sum_ += (my_dict[k] - (threes * 3) - (fivers * 5)) * 50
         if k == "B":
-            
-
+            twos = my_dict[k] % 2
+            sum_ += twos * 45
+            single_b = my_dict[k] - (twos * 2)
+            sum_ += (my_dict[k] - (twos * 2)) * 30
+        if k == "C":
+            sum_ += my_dict[k] * 20
+        if k == "D":
+            sum_ += my_dict[k] * 15
+        if k == "E":
+            sum_ = my_dict[k] * 40
+            two_e = my_dict[k] % 2
+            sum_ -= single_b * 30
 
     return int(sum_)
