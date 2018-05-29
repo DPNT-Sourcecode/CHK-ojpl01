0dcf52877b7fb17f69bc042a99cf1d89f84e672b
sbx_user1060 <sbx_user1060@ip-10-12-61-131.eu-west-2.compute.internal>
2018-05-29 19:59:19 +0000

    Tue May 29 19:59:19 UTC 2018

--- lib/solutions/checkout.py
+++ lib/solutions/checkout.py
@@ -6,33 +6,38 @@
 def checkout(skus):
 
     sum_ = 0
-    a = 0
-    b = 0
-    e = 0
-    for item in skus:
+    my_dict = {}
 
-        if item == "A" and a != 2:
-            sum_ += 50
-            a += 1
-        elif item == "B" and b != 1:
-            sum_ += 30
-            b += 1
+    for item in skus:
+        if item == "A":
+            my_dict.update({item: mydict[item]+1})
+        elif item == "B":
+            my_dict.update({item: mydict[item]+1})
         elif item == "C":
-            sum_ += 20
+            my_dict.update({item: mydict[item]+1})
         elif item == "D":
-            sum_ += 15
-        elif item == "E" and e != 1:
-            sum_ += 40
-            e += 1
+            my_dict.update({item: mydict[item]+1})
+        elif item == "E":
+            my_dict.update({item: mydict[item]+1})
         elif item == "A" and a == 2:
-            sum_ += 30
-            a = 0
+            my_dict.update({item: mydict[item]+1})
         elif item == "B" and b == 1:
-            sum_ += 15
-            b = 0
+            my_dict.update({item: mydict[item]+1})
         elif item == "E" and e == 1:
-            sum_ -= 30
-            e = 0
+            my_dict.update({item: mydict[item]+1})
         else:
             return -1
+
+    offers = 0
+    for k, v in my_dict:
+        if k == "A":
+            fivers = my_dict[k] % 5
+            sum_ += fivers * 200
+            threes = fivers % 3
+            sum_ += threes * 130
+            sum_ += (my_dict[k] - (threes * 3) - (fivers * 5)) * 50
+        if k == "B":
+            
+
+
     return int(sum_)
